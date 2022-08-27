709890f0774b770a136c833f04683204360c94e5
DucLe97 <leducst1@gmail.com>
2022-08-28 02:05:01 +0700

    submit

--- provider.tf
+++ provider.tf
@@ -4,3 +4,12 @@ provider "azurerm" {
 version = "=2.0.0"
 features {}
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
