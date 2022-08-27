dc42b9326528dc0a710d4f51f62b16d06e5765de
DucLe97 <leducst1@gmail.com>
2022-08-27 22:21:34 +0700

    submit

---
+++ .history/compute_20220827213928.tf
@@ -0,0 +1,87 @@
+resource "azurerm_public_ip" "pip" {
+  name                = "publicip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku = "Basic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "VM-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "Ubuntu-K8S"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  vm_size               = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name = "book-osdisk"
+
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "VMBOOK"
+    admin_username = "Administrator1"
+    admin_password = "P@ssw0rd111"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "Ubuntu"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
--- .history/compute_20220827221702.tf
+++ .history/compute_20220827221702.tf
@@ -19,6 +19,45 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "Ubuntu-K8S"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  vm_size               = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name = "book-osdisk"
+
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "VMBOOK"
+    admin_username = "Administrator1"
+    admin_password = "P@ssw0rd111"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "linux_VM" {
   name                = "Ubuntu"
   resource_group_name = azurerm_resource_group.rg.name
@@ -26,22 +65,26 @@ resource "azurerm_linux_virtual_machine" "linux_VM" {
   size                = "Standard_DS1_v2"
   admin_username      = "adminuser"
   admin_password = "P@ssw0rd111"
-  disable_password_authentication = false
+  disable_password_authentication = true
 
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
 
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
   os_disk {
-    name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 }
